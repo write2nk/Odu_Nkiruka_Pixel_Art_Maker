53c6b7ce9061f112256230773b16def50e781f3a
write2nk <40614976+write2nk@users.noreply.github.com>
2018-06-29 14:33:47 -0700

    Update design.js

--- design.js
+++ design.js
@@ -1,18 +1,37 @@
-// Select color input
-// Select size input
 
 // When size is submitted by the user, call makeGrid()
-function makeGrid() {
-   
-
+function makeGrid(rows, columns) {
 // Your code goes here!
-var rows = $('')
-
+for(let i=1; i<=rows; i++){
+    //selects the table and appends table rows depending on the user input
+    var tab = $('table');
+    tab.append('<tr></tr>');
 }
-// Select color input
-
+//selects the tag table row<tr> to append the column 
+    for(let i=1; i<=columns; i++){ 
+        $('tr').append('<td></td>');
+    }
+}
 // Select size input
+// When the height and width size is submitted by the user, calls makeGrid()
+$('form').submit(function(event){
+    let rows = $('input#inputHeight').val();
+    let columns = $('input#inputWidth').val();
 
+    // clears the DOM to append the next <tr><td>
+    $('tr').remove();
+    //calls the makeGrid() function
+    makeGrid(rows, columns);
+    event.preventDefault();
+});
 
-
-// When size is submitted by the user, call makeGrid()
+//Get the color value as the user input changes
+// Select color input as the
+$('#colorPicker').change(function(event){
+    let gridColor = $(this).val();
+    $('td').click(function(event){
+        $(this).css("background-color", gridColor); 
+    });
+        
+    event.preventDefault();
+});
