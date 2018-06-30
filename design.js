e27cf9396bc44da74f228454483762eb4a8d85f4
write2nk <40614976+write2nk@users.noreply.github.com>
2018-06-30 16:16:19 -0700

    Update design.js

--- design.js
+++ design.js
@@ -1,11 +1,11 @@
 
+
 // When size is submitted by the user, call makeGrid()
 function makeGrid(rows, columns) {
 // Your code goes here!
 for(let i=1; i<=rows; i++){
 //selects the table and appends table rows depending on the user input
-    var tab = $('table');
-    tab.append('<tr></tr>');
+    const tab = $('table').append('<tr></tr>');
 }
 //selects the tag table row<tr> to append the column<td>
     for(let i=1; i<=columns; i++){ 
@@ -15,8 +15,8 @@ for(let i=1; i<=rows; i++){
 
 // When the height and width size is submitted by the user, calls makeGrid()
 $('form').submit(function(event){
-    let rows = $('input#inputHeight').val();
-    let columns = $('input#inputWidth').val();
+    let rows = $('#inputHeight').val();
+    let columns = $('#inputWidth').val();
 
 // clears the DOM to append the next <tr><td>
     $('tr').remove();
